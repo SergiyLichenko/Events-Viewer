4e24a43b1d17cee39432b3b49d3c38cc916039ad
SergiyLichenko <sergiy.lichenko@outlook.com>
2018-01-07 09:06:56 -0800

    Added Toastr package

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -1,20 +1,27 @@
 import { Component } from "@angular/core";
 import { EventService } from "./shared/event.service";
 import { OnInit } from "@angular/core";
+import { ToastrService } from "../common/toastr.service";
 
 @Component({
   selector: 'events-list',
-  templateUrl: 'app/events/events-list.component.html'
+  templateUrl: 'app/events/events-list.component.html',
+  providers: [ToastrService]
 })
 
 export class EventListComponent implements OnInit {
   events: any[];
 
-  constructor(private _eventService: EventService) {
+  constructor(private _eventService: EventService,
+    private _toastrService: ToastrService) {
 
   }
 
   ngOnInit() {
     this.events = this._eventService.getEvents();
+  }
+
+  handleThumbnailClick(eventName: string) {
+    this._toastrService.info(eventName);
   }
 }
